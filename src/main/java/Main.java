07610aef382b82c7e893fbde23dbf0fc459c7cba
Paweł <pawel_wis@yahoo.com>
2019-06-23 13:39:41 +0200

    Use Port Scanner in Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public void main(String []args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type down port: ");
+        int port_min = in.nextInt();
+        System.out.println("Type up port: ");
+        int port_max = in.nextInt();
+        System.out.println("Type host: ");
+        String host = in.nextLine();
+
+        Map<Integer, Boolean> open_ports = new HashMap<>();
+
+        try {
+            open_ports = new PortScanner(host, port_min, port_max).scan();
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        String port_status;
+        for (Map.Entry<Integer, Boolean> pair : open_ports.entrySet()){
+            port_status = pair.getValue() ? "open" : "close";
+            System.out.println("Port: " + pair.getKey() + " is " + port_status);
+        }
+    }
+}
