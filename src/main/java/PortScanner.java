beaa5725a1eb2ef4c2f35c08dbb6330070e4b942
Paweł <pawel_wis@yahoo.com>
2019-06-23 13:22:20 +0200

    Add scan method

--- src/main/java/PortScanner.java
+++ src/main/java/PortScanner.java
@@ -1,14 +1,19 @@
 import java.awt.*;
+import java.awt.image.AreaAveragingScaleFilter;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PortScanner {
     private URL url;
     private int[] port_range;
     private int single_port;
+    private int timeout = 1000;
 
     public PortScanner(String url, int port_min, int port_max) throws MalformedURLException {
         this.url = new URL(url);
@@ -20,19 +25,21 @@ public class PortScanner {
         this.single_port = single_port;
     }
 
-    public ArrayList<Boolean> scan(){
+    public Map<Integer, Boolean> scan(){
+        Map<Integer, Boolean> open_ports = new HashMap<>();
 
-        // It should return table of open ports, propably it will be keys[port number] and value[state]
-
-        for(int i=0; i < port_range.length; i++){
+        for (int port : port_range){
             try{
                 Socket socket = new Socket();
-                socket.connect(new InetSocketAddress(this.url.getPath(), port_range[i]), 1000);
+                socket.connect(new InetSocketAddress(this.url.getPath(), port), this.timeout);
                 socket.close();
-
+                open_ports.put(port, true);
+            } catch(Exception e){
+                open_ports.put(port, false);
             }
         }
 
+        return  open_ports;
     }
 
 
